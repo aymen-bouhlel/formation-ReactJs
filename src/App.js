2938398679f25cf82950bddb8b97a1897121ee61
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-02-08 11:23:27 +0100

    Stateless

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
+import Member from './components/Membre'
 
 function App () {
   return (
     <div className='App'>
       <h1>React App</h1>
+      <Member />
     </div>
   )
 }
---
+++ src/components/Membre.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Membre = () => <h2>Membre</h2>
+
+export default Membre
