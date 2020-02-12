2f7efb55f3b9e37f85fa866f4f3c10466a145e81
Bouhlel El Bechri <bouhlel_elbechri@web.de>
2020-02-12 08:33:04 +0100

    Utiliser le style inline

--- src/components/Membre.js
+++ src/components/Membre.js
@@ -3,7 +3,7 @@ import React, { Fragment } from 'react'
 const Membre = ({ nom, age }) => {
   return (
     <Fragment>
-      <h2>{nom.toUpperCase()} : {age}</h2>
+      <h2 style={{ backgroundColor:'purple', color:'white' }}>{nom.toUpperCase()} : {age}</h2>
     </Fragment>  
   )
 }
